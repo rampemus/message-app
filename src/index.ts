4cdef87f91fa75a1693712c1d079c02b5cb6403b
rampemus <pasi.toivanen@live.com>
2020-08-22 22:06:53 +0300

    Install express types and init server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { MikroORM } from "@mikro-orm/core"
 import { ___prod___ } from './constants'
-import { Post } from './entities/Post'
 import mikroConfig from "./mikro-orm.config"
+import express from "express";
 // import path from 'path'
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig)
     await orm.getMigrator().up()
+
     // const post = orm.em.create(Post, { title: 'my first post' })
     // await orm.em.create(Post, { title: 'My first post' })
     // await orm.em.persistAndFlush(post)
-    const posts = await orm.em.find(Post, {})
-    console.log('posts:', posts)
+
+    const app = express()
+    app.listen(3000, () => {
+        
+    })
 }
 
 console.log('Hello world!')
