1ad593a058d5fda20cd12c3e6aa3309fc71c9c94
rampemus <pasi.toivanen@live.com>
2020-08-22 22:02:22 +0300

    Log all data

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { MikroORM } from "@mikro-orm/core"
 import { ___prod___ } from './constants'
 import { Post } from './entities/Post'
-import mikroConfig from "./mikro-orm.config";
+import mikroConfig from "./mikro-orm.config"
 // import path from 'path'
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig)
-
-    const post = orm.em.create(Post, { title: 'my first post' })
-    // console.log('--------------sql---------------')
+    await orm.getMigrator().up()
+    // const post = orm.em.create(Post, { title: 'my first post' })
+    // await orm.em.create(Post, { title: 'My first post' })
     // await orm.em.persistAndFlush(post)
+    const posts = await orm.em.find(Post, {})
+    console.log('posts:', posts)
 }
 
-console.log('Hello world! Hello')
+console.log('Hello world!')
 
 main()
